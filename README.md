2f4bf6401dec8776a5f867ab5f24d8e66ad8ba5f
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-12-30 17:38:02 +0100

    last version

---
+++ README.md
@@ -0,0 +1,14 @@
+### Calculator
+
+ Create the class Calculator
+ this class has 3 methods:
+ - history: once the = key is pressed, it keeps the last value in memory in an array
+ - equals: returns the value (thanks to an 'eval' function)
+ - clear: clears the history
+
+
+ This function below write the value of the pressed key on the screen
+ The += is the equivalent of:
+
+Code listen to every key on the calculator and add the value on the screen
+
--- js/script.js
+++ js/script.js
@@ -8,32 +8,41 @@
  */
 
 const calculatorScreen = document.querySelector("#calculator .screen");
-const equals = document.querySelector("#calculator .eval");
+const equalsHTMLSelector = document.querySelector("#calculator .eval");
+const historyUL = document.querySelector('#history')
 
 class Calculator {
-    constructor(history, equals, clear) {
-        this._history = [];
-        this._equals = eval;
-        this._clear = 0;
-    }
-    /*  clear() {
-         this.history.length = 0;
-     } */
-}
+    constructor() {
 
+        //State => stuff that chages
+        this._history = [];
+    }
+    //push date in empty array
+    history() {
+        this._history.push(calculatorScreen.innerHTML)
+        historyUL.innerHTML += `<li>${calculatorScreen.innerHTML} = ${this.equals()}</li>`
+    }
+    // add calculation to this ._history
+    // add current calculatio to #history UL
+    equals() {
+        return eval(calculatorScreen.innerHTML.replaceAll("x", "*"))
+    }
+    clear() {
+        // this._history = [];
+        calculatorScreen.innerHTML = '';
+    }
+}
+const myCalculator = new Calculator();
 /**
  * This function below write the value of the pressed key on the screen
  * The += is the equivalent of:
  * document.querySelector('.screen').innerHTML = document.querySelector('.screen').innerHTML + val;
  *
  **/
+
 function print(val) {
-    if (operatorPressed) {
-        calculatorScreen.innerHTML = "";
-        operatorPressed = false;
-    }
     calculatorScreen.innerHTML += val;
-    valuesArray.push(val);
+
 }
 
 //this code listen to every key on the calculator and add the value on the screen
@@ -42,42 +51,16 @@ document.querySelectorAll("#calculator span").forEach((key) => {
         key.addEventListener("click", (e) => print(e.target.innerText));
     }
 });
-document.querySelector("#calculator .clear").addEventListener("click", () => {
-    calculatorScreen.innerHTML = "";
-    storedValue = 0;
-    valuesArray = [];
-    lastValue = 0;
+document.querySelector("#calculator .clear").addEventListener("click", () => (calculatorScreen.innerHTML = ""));
+
+
+
+// Implement here the event when the = key is pressed
+equalsHTMLSelector.addEventListener('click', () => {
+    myCalculator.history()
+    calculatorScreen.innerHTML = myCalculator.equals()
 });
-// "+" operator
-document.querySelector(".plus").addEventListener("click", () => {
-    evaluateOperations();
-    operatorPressed = true;
-    lastOperator = "+";
-});
-// "-" operator
-document.querySelector(".minus").addEventListener("click", () => {
-    evaluateOperations();
-    operatorPressed = true;
-    lastOperator = "-";
-});
-// "x" operator
-document.querySelector(".multiply").addEventListener("click", () => {
-    evaluateOperations();
-    operatorPressed = true;
-    lastOperator = "*";
-});
-// "/" operator
-document.querySelector(".divide").addEventListener("click", () => {
-    evaluateOperations();
-    operatorPressed = true;
-    lastOperator = "/";
-});
-//"=" operator
-document.querySelector(".eval").addEventListener("click", () => {
-    if (storedValue) {
-        evaluateOperations();
-        operatorPressed = true;
-        lastOperator = "=";
-    }
-});
- // Implement here the event when the = key is pressed
+
+window.onload = function () {
+    calculatorScreen.focus();
+}
